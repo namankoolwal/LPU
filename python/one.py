210cbbc7c1733650a4ab58cebf5ce0e0f6361aa7
Naman khandelwal <namankoolwal1411@gmail.com>
2023-12-03 11:03:26 +0530

    add one

---
+++ python/one.py
@@ -0,0 +1,35 @@
+# The code is assigning the value 100 to the variable `x`, the value 200 to the variable `y`, and then
+# adding `x` and `y` together and assigning the result to the variable `z`. Finally, it prints the
+# value of `z`, which is the sum of `x` and `y`.
+
+x= 11;
+y= 11;
+
+z=x
+# z= x >> 2;
+# print(z)
+
+# The code you mentioned is commented out, which means it is not being executed. However, if you
+# uncomment it by removing the `#` symbols, it will perform the following actions:
+
+# print ("address of x = " , id(x))
+# print ("address of y = " , id(y))
+
+# if x is z :
+#     # print("both are having same value")
+# else:
+#     # print("both are having different value")
+
+
+
+# membership operator
+
+# The code is creating a list `a` with the values `[1, 2, 3, 4, 55, 66, 77, 100]`. Then, it checks if
+# the value `4` is present in the list `a` using the `in` operator. The result of this check is
+# assigned to the variable `z`. Finally, it prints the value of `z`, which will be `True` if `4` is in
+# the list `a`, and `False` otherwise.
+a = [1, 2, 3, 4, 55, 66, 77, 100]
+print(a)
+
+z= 4 in a
+print(z)
