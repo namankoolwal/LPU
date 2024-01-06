d8ef3c6c1dce0dac309399a86a63d92162b5bf9b
Naman khandelwal <namankoolwal1411@gmail.com>
2024-01-06 14:58:28 +0530

    1/6/24-live class

--- PHP/threString.php
+++ PHP/threString.php
@@ -1,8 +1,14 @@
 <?php
 
-$string1 = "Hello";
-$string2 = "World";
-$string3 = "Good Afternoon";
+// $string1 = "Hello";
+// $string2 = "World";
+// $string3 = "Good Afternoon";
 
-echo $string1 . " " . $string2 . " " . $string3;
+// echo $string1 . " " . $string2 . " " . $string3;
+
+
+$a = 1234;
+
+ var_dump($a);
+
 ?>
